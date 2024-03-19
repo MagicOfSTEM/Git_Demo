7dcbb060b7046cedfb418e0d2d32dfd635af84df
MagicOfSTEM <153861191+MagicOfSTEM@users.noreply.github.com>
2024-03-19 16:51:52 +0000

    Initial commit: Version 1.0

---
+++ Try_Git.py
@@ -0,0 +1,22 @@
+# Simple Calculator for Git
+
+print('Welcome to this simple calculator')
+
+number1 = float(input("Please enter your 1st number: "))
+number2 = float(input("Please enter your 2nd number: "))
+operator = input("Please enter the operator/math function: +, -, x, /:  " )
+
+def calc(num1, num2, op):
+        if op == '+':
+            answer = num1 + num2
+        elif op == '-':
+            answer = num1 - num2
+        elif op == 'x':
+            answer = num1 * num2
+        elif op == '/':
+            answer = num1 / num2
+        else:
+            print("Inputs not recognised, please try again")
+        return print(answer)
+    
+calc(number1, number2, operator)
